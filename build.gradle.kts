51132afe01566feb74b95a219ecaa6d48675d6d9
pkostrygin <pkostrygin@mobiledimension.ru>
2021-11-08 15:45:51 +0300

    junit added to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.5.31"
     application
 }
 
@@ -12,7 +12,15 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
+    implementation("org.jetbrains.kotlin:kotlin-test:1.5.31")
+    implementation("org.jetbrains.kotlin:kotlin-test-junit5:1.5.31")
+}
 
+tasks.test {
+    useJUnitPlatform()
+}
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
