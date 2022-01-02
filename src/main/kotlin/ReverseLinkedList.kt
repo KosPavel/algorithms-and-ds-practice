fa2781accb8a1a4382b019d01c81d9cab6079f81
pkostrygin <pkostrygin@mobiledimension.ru>
2022-01-02 14:17:00 +0300

    ReverseLinkedList implemented

---
+++ src/main/kotlin/ReverseLinkedList.kt
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test
+
+//https://leetcode.com/problems/reverse-linked-list/
+class ReverseLinkedList {
+
+    fun reverseList(head: ListNode?): ListNode? {
+        if (head == null) return null
+
+        return if (head.next == null) head
+        else {
+            val newNode = reverseList(head.next)
+            head.next!!.next = head
+            head.next = null
+            newNode
+        }
+    }
+
+}
+
+class ReverseLinkedListTest {
+
+    private val reverseLinkedList = ReverseLinkedList()
+
+    @Test
+    fun searchInsertTest1() {
+        val inputNode = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+        val expectedNode = ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1)))))
+        reverseLinkedList.reverseList(inputNode).assertNodesEquals(expectedNode)
+    }
+
+    @Test
+    fun searchInsertTest2() {
+        val inputNode = ListNode(1, ListNode(2, ListNode(3)))
+        val expectedNode = ListNode(3, ListNode(2, ListNode(1)))
+        reverseLinkedList.reverseList(inputNode).assertNodesEquals(expectedNode)
+    }
+
+    @Test
+    fun searchInsertTest3() {
+        val inputNode = ListNode(1, ListNode(2))
+        val expectedNode = ListNode(2, ListNode(1))
+        reverseLinkedList.reverseList(inputNode).assertNodesEquals(expectedNode)
+    }
+
+    @Test
+    fun searchInsertTest4() {
+        val inputNode = null
+        val expectedNode = null
+        reverseLinkedList.reverseList(inputNode).assertNodesEquals(expectedNode)
+    }
+
+}
